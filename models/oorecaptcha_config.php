8e4696634af76901a33e9b0634b391106cc422ad
Jonathan Garbee <jonathan@garbee.me>
2013-04-12 13:23:47 -0300

    Update oorecaptcha_config.php
    Added tabs to separate regular recaptcha and the new mailhide keys. Also (ofc) added mailhide API key entries.

--- models/oorecaptcha_config.php
+++ models/oorecaptcha_config.php
@@ -3,17 +3,23 @@
     {
         public $record_code = 'oorecaptcha_config';
 
-        public static function create() {
+        public static function create()
+        {
             $config = new self();
 
             return $config->load();
         }
 
-        protected function build_form() {
-            $this->add_field('public_key', 'Public Key', 'full', db_varchar)->comment('Specify the public key .', 'above')->validation()->fn('trim')->required('Please specify the public key.');
-            $this->add_field('private_key', 'Private Key', 'full', db_varchar)->comment('Specify the private key for communicating with the recaptcha server.', 'above')->validation()->fn('trim')->required('Please specify the private key.');
+        protected function build_form()
+        {
+            $this->add_field('public_key', 'Public Key', 'full', db_varchar)->tab('Recaptcha')->comment('Specify the public key .', 'above')->validation()->fn('trim')->required('Please specify the public key.');
+            $this->add_field('private_key', 'Private Key', 'full', db_varchar)->tab('Recaptcha')->comment('Specify the private key for communicating with the recaptcha server.', 'above')->validation()->fn('trim')->required('Please specify the private key.');
+
+            $this->add_field('mailhide_pubkey', 'Mailhide Public Key', 'full', db_varchar)->tab('Mailhide')->comment('Specify the mailhide public key.', 'above')->validation()->fn('trim');
+            $this->add_field('mailhide_privkey', 'Mailhide Private Key', 'full', db_varchar)->tab('Mailhide')->comment('Specify the mailhide private key.', 'above')->validation()->fn('trim');
         }
 
-        protected function init_config_data() {
+        protected function init_config_data()
+        {
         }
     }
